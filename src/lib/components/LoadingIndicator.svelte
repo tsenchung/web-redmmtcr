75dbc81fe625fdbe32fad673d7fa772381dbc692
Tsen Chung <tsen.fchung@gmail.com>
2022-01-16 01:27:23 +0000

    Loading indicator

---
+++ src/lib/components/LoadingIndicator.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { navigating } from '$app/stores';
+  import { fade } from 'svelte/transition';
+</script>
+
+{#if $navigating}
+  <div transition:fade={{ duration: 100 }} class="progress" />
+{/if}
